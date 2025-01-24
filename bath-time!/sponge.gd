f613b2dd96bee6e29f8c13294b7248f3768fd7a0
ImpersonatingRooster <xt1xex@gmail.com>
2025-01-24 18:54:29 -0400

    First sponge interation

---
+++ bath-time!/sponge.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var mouse_position = get_global_mouse_position()
+	position = mouse_position
