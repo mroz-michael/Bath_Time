4940dfbb95e051eb49e3ed5c7e5245c043a55837
ImpersonatingRooster <xt1xex@gmail.com>
2025-01-24 21:23:46 -0400

    sponge disapears when goal is reached

--- bath-time!/main.gd
+++ bath-time!/main.gd
@@ -4,16 +4,21 @@ var clean_goal = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	new_game()
 	
 func _on_cleaning(amount: float):
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if (clean_progress > clean_goal):
+		game_over()
 
-
+func new_game():
+	clean_progress = 0
+	$Sponge.show()
+func game_over():
+	$Sponge.hide()
 func _on_sponge_cleaning(amount: float) -> void:
 	clean_progress += amount
 	print(clean_progress)# Replace with function body.
--- bath-time!/sponge.gd
+++ bath-time!/sponge.gd
@@ -9,7 +9,7 @@ var real_score_multiplier = score_multiplier * 250
 signal cleaning(amount: float)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	hide()# Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
