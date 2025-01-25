48b1933f9a5e8652ede7f162c78febe09f82ad98
ImpersonatingRooster <xt1xex@gmail.com>
2025-01-25 11:57:35 -0400

    implemented clean bar

--- bath-time!/main.gd
+++ bath-time!/main.gd
@@ -5,6 +5,7 @@ var clean_goal = 100
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	new_game()
+	$mainSceneHud/CleanBar.value = clean_progress
 	
 func _on_cleaning(amount: float):
 	pass
@@ -13,12 +14,14 @@ func _on_cleaning(amount: float):
 func _process(delta: float) -> void:
 	if (clean_progress > clean_goal):
 		game_over()
+	$mainSceneHud/CleanBar.value = clean_progress
 
 func new_game():
 	clean_progress = 0
 	$Sponge.show()
 func game_over():
 	$Sponge.hide()
+	$mainSceneHud/CleanBar.hide()
 func _on_sponge_cleaning(amount: float) -> void:
 	clean_progress += amount
 	print(clean_progress)# Replace with function body.
